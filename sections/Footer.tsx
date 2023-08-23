74621e3aeb0b232aab4a77828eb98a41142a6fa9
Battlearmour2000 <61649287+Battlearmour2000@users.noreply.github.com>
2023-08-23 14:18:00 +0300

    Jainick 1st attempt

--- sections/Footer.tsx
+++ sections/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="py-6 text-center text-gray-500 text-sm">
       <span className="dark:text-gray-100 text-gray-900 font-bold text-lg mr-2">
-        Fredy
+        PesaLock
       </span>
       &copy; {new Date().getFullYear()} All Rights Reversed
     </footer>
--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -1,4 +1,5 @@
 import Button from "../components/Button";
+import iphone from "../public/iphone-13.jpeg"
 
 const Hero = () => {
   return (
@@ -7,17 +8,36 @@ const Hero = () => {
         Hi Im <span className="dark:text-purple-600">Fredy</span>
       </h1>
       <h3 className="text-4xl my-3">I am Web Designer</h3>
-      <p className="text-gray-700 mb-8">
+      <p className="text-700 mb-8">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
         quibusdam autem doloremque beatae iure, nihil fugit doloribus cum soluta
         modi!
       </p>
+      
+      
       <Button
-        className="bg-purple-600 text-white px-6"
+        className="bg-purple-600 text-white px-6 margin-left: 0;"
         onClick={() => console.log("clicked")}
       >
         Hire Me!
       </Button>
+      <img></img>
+
+      <div className="flex-container">
+                <nav className="nav-bar">
+                    <ul>
+                        <li>
+                          
+                          <img src="resources/Home.svg"></li>
+                        <li><img src="resources/Orders.svg"></li>
+                        <li><img src="resources/Products.svg"></li>
+                        <li><img src="resources/profile.svg"></li>
+                        <li><img src="resources/Statistics.svg"></li>
+                    </ul>
+                </nav>
+                <div className="">Lorem  rijgnerngkdjgrgrgr</div>
+                <div className="">uenhfunhwrjbgr3</div>
+            </div>
     </section>
   );
 };
